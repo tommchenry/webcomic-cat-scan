a166db22806d13e69f718555cd6047fe757948af
Tom McHenry <tommchenry@gmail.com>
2017-01-15 20:27:45 -0600

    Strip out personalized code.

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -1,11 +1,8 @@
 class Comic < ActiveRecord::Base
-<<<<<<< HEAD
   before_validation :slug_url, :add_root_url, :ensure_post_date
-=======
   before_validation :slug_url, :add_root_url
->>>>>>> Add root url.
 
-  ROOT_URL = "http://www.noncanon.online/comics/"
+  ROOT_URL = ""
 
   def slug_url
     unless self.url_slug
@@ -14,7 +11,9 @@ class Comic < ActiveRecord::Base
   end
 
   def add_root_url
-    self.img_url = File.join(ROOT_URL, self.img_url)
+    unless self.img_url =~ /^(http|https):\/\//
+      self.img_url = File.join(ROOT_URL, self.img_url)
+    end
   end
 
   def ensure_post_date
